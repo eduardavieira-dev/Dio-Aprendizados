369e956bbdbc902ec621e99b7e51cd7af914e3a1
Eduarda Vieira <eduarda.vieira.goncalves7@gmail.com>
2024-06-29 00:36:17 -0300

    auto-preenchimento-json

--- JavaScript/Portifolio-Dio/assets/js/api.js
+++ JavaScript/Portifolio-Dio/assets/js/api.js
@@ -1,7 +1,7 @@
 //Fazer um fetch que que retorna uma promise
 
 async function fetchProfileData(){
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/eduardavieira-dev/Dio-Aprendizados/main/JavaScript/Portifolio-Dio/data/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
